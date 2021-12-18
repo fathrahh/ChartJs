77522221c6b89f063d9eb086790922b879654ff8
fathrahh <gazoon3@gmail.com>
2021-12-18 08:50:11 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./public/index.html", "./public/template.html"],
+  theme: {
+    extend: {
+      colors: {
+        'navbar': '#4e4f57',
+        'section': '#363740',
+        'aside': '#1F1F21'
+      },
+      spacing: {
+        '20vw': '20vw'
+      }
+    },
+  },
+  plugins: [],
+}
